38d666f8d16ad88ed9c5237c2a466bfc8489bf1d
vv33555 <vv33555@163.com>
2023-05-02 13:24:23 +0800

    update output.py

---
+++ examples/output.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+import glob
+
+output_dir = [x for x in os.listdir('.') if os.path.isdir(x) and 'output_' in x]
+
+try:
+    os.mkdir("output")
+except:
+    pass
+
+for i in output_dir:
+    for j in glob.glob(os.path.join(i,"output_top","output_pdbqt","*__input_top1.pdbqt")):
+        os.rename(j,os.path.join(i,"output_top","output_pdbqt",os.path.basename(j).split("__")[0]+"_gyp.pdbqt"))
+    for j in glob.glob(os.path.join(i,"output_top","output_pdbqt","*_gyp.pdbqt")):
+        shutil.move(j,os.path.join("output",os.path.basename(j)))
+        
